7cfbce2e367892e31e084acfc1b822118e69b42f
Tahaberk <berksysl.g@gmail.com>
2022-12-13 23:40:49 +0300

    First version completed

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const amountEl = document.querySelector('#amount');
-const fromCur = document.querySelector('#fcurrencies [selected]');
+const fromCur = document.querySelector('#fcurrencies');
+const fromVal = document.querySelectorAll('#fcurrencies option')
 const changeBtn = document.querySelector('.changeBtn');
 const result = document.querySelector('.result h2');
-const toCur = document.querySelector('#tcurrencies [selected]');
+const toCur = document.querySelector('#tcurrencies');
+const toVal = document.querySelectorAll('#tcurrencies option')
 const submit = document.querySelector('button');
+const replaceBtn = document.querySelector('.changeBtn');
 const myHeaders = new Headers();
 let to;
 let from;
@@ -19,13 +22,14 @@ const requestOptions = {
 
 function getInputs() {
     amount = amountEl.value;
-    from = fromCur.value;
-    to = toCur.value;
+    from = fromVal[fromCur.selectedIndex].value;
+    to = toVal[toCur.selectedIndex].value;
 }
 
 
 submit.addEventListener("click", function(){
     getInputs();
+    result.innerHTML = "Getting rates...";
     console.log("amount: " + amount + " from: " + from + " to : " + to);
     fetch(`https://api.apilayer.com/fixer/convert?to=${to}&from=${from}&amount=${amount}`, requestOptions)
   .then(response => response.json())
@@ -33,3 +37,10 @@ submit.addEventListener("click", function(){
   .catch(error => console.log('error', error));
 
 });
+
+changeBtn.addEventListener("click", () => {
+    let oldVal = fromCur.selectedIndex;
+    fromCur.selectedIndex = toCur.selectedIndex;
+    toCur.selectedIndex = oldVal;
+    console.log(fromCur.selectedIndex + " " + toCur.selectedIndex);
+})
